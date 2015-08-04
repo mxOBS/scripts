ad43945084c3cc8d1404c1448524edfed614900b
Josua Mayer <privacy@not.given>
2015-08-04 22:19:24 +0000

    implement checksum support for rpm repos

--- fetch-rpm-repo.sh
+++ fetch-rpm-repo.sh
@@ -4,38 +4,96 @@ usage() {
 	echo "Usage: $0 <primary.xml> <mirror> <destdir>"
 }
 
-strip_xml() {
-	while read line; do
+read_file() {
+	uri="$1"
+	if [[ $uri = http://* ]]; then
+		cmd="curl -k $uri 2>/dev/null"
+	else
+		cmd="cat $uri"
+	fi
 
-		# remove first part
-		pkg=`echo $line | sed -e 's;^**<location href=\";;g'`
-
-		# remove last part
-		pkg=`echo $pkg | sed -e 's;\"/>.*;;g'`
-
-		echo $pkg
-	done
+	# unpackif necessary
+	if [[ $uri = *.gz ]]; then
+		eval $cmd | gzip -d
+	else
+		eval $cmd
+	fi
 }
 
-prepend_mirror() {
+extract_data() {
+	name=
+	chksum=
+	chksumtype=
+	location=
 	while read line; do
-		echo "$mirror/$line"
+		if [[ $line = \<package\ * ]]; then
+			name=
+			chksum=
+			chksumtype=
+			location=
+		fi
+		if [[ $line = \<name* ]]; then
+			name=$(echo $line | sed -e "s;<name>;;g" -e "s;</name>;;g")
+
+			continue
+		fi
+		if [[ $line = \<checksum*sha256* ]]; then
+			chksum=$(echo $line | sed -e 's;<checksum type="sha256" pkgid="YES">;;g' -e "s;</checksum>;;g")
+			chksumtype=sha256
+
+			continue
+		fi
+		if [[ $line = \<location* ]]; then
+			location=$(echo $line | sed -e "s;<location href=;;g" -e "s;/>;;g" -e 's;";;g')
+
+			continue
+		fi
+		if [[ $line = \</package\> ]]; then
+			echo $name $mirror/$location $chksumtype $chksum
+
+			continue
+		fi
 	done
 }
 
 download() {
-	while read url; do
-		name=`basename $url`
+	while read line; do
+		name=$(echo $line | cut -d' ' -f1)
+		location=$(echo $line | cut -d' ' -f2)
+		chksumtype=$(echo $line | cut -d' ' -f3)
+		chksum=$(echo $line | cut -d' ' -f4)
+
+		filename=`basename $location`
 		printf "%s: " $name
 
-		if [ -e "$destdir/$name" ]; then
-			# TODO: check if md5 matches
+		if [ -e "$destdir/$filename" ]; then
+			# check if chksum matches
+			matches=no
+			if [ "$chksumtype" = "sha256" ]; then
+				echo "$chksum $destdir/$filename" | sha256sum -c 2>/dev/null 1>/dev/null
+				if [ $? = 0 ]; then
+					matches=yes
+				fi
+			fi
 
-			# skip this package
-			status="Skipped"
+			# if checksum matches, keep the file
+			if [ "$matches" = "yes" ]; then
+				status="Skipped"
+			else
+				# get rid of the bad file
+				rm -f $filename
+
+				# then download
+				curl -k -o "$destdir/$filename" "$location" 1>/dev/null 2>/dev/null
+				if [ $? == 0 ]; then
+					status="Replaced"
+				else
+					status="Failed"
+				fi
+			fi
 		else
 			# download
-			curl -k -o "$destdir/$name" "$url" 1>/dev/null 2>/dev/null
+			curl -k -o "$destdir/$filename" "$location" 1>/dev/null 2>/dev/null
 			if [ $? == 0 ]; then
 				status="Done"
 			else
@@ -55,4 +113,5 @@ metadata="$1"
 mirror="$2"
 destdir="$3"
 
-grep "<location href=" "$metadata" | strip_xml | prepend_mirror | download
+#read_file "$metadata" | grep "<location href=" | strip_xml | prepend_mirror | download
+read_file "$metadata" | extract_data | download
